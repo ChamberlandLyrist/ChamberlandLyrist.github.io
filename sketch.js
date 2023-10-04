0c81156d779f73baf95fee01e27d14495d6832ca
ChamberlandLyrist <146767709+ChamberlandLyrist@users.noreply.github.com>
2023-10-04 11:36:48 -0600

    added ball demo?

---
+++ sketch.js
@@ -0,0 +1,18 @@
+// le-ball
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let d = 20; 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX, mouseY, d);
+}
