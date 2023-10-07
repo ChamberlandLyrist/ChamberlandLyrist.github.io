7697275df2cd3f1c8abf48b67d423b14babbdca9
ChamberlandLyrist <146767709+ChamberlandLyrist@users.noreply.github.com>
2023-10-07 15:15:02 -0600

    Started making functioning collision system using Metter.js (under heavy guidance of ChatGPT ugh)

--- tds-card/tds.js
+++ tds-card/tds.js
@@ -1,5 +1,13 @@
 
 
+
+// NOTE:
+// I couldn't get collisions working: so anything that uses Matter.js stuff was from ChatGPT, UNLESS OTHERWISE SPECIFIED
+
+
+
+
+
 // assume that all sizes are for a screen of 20 units
 // ex: size 0.5 should be 0.5/20 units of size
 let player = {
@@ -9,27 +17,31 @@ let player = {
   gun: [],
   x: 50,
   y: 50,
-  speed: 0.5/20
+  speed: 1/20
 };
 
+let aK = 65;
+let dK = 68;
+let wK = 87;
+let sK = 83;
 function moveP(){
   let velx = 0;
   let vely = 0;
-  let a = keyIsDown(65);
+  let a =keyIsDown(aK);
+  let d = keyIsDown(dK);
+  let w =keyIsDown(wK);
+  let s =keyIsDown(sK);
   if (a){
-    velx -= player.speed;
+    velx -= 1;
   }
-  let d = keyIsDown(68);
   if (d){
-    velx += player.speed;
+    velx += 1;
   }
-  let w = keyIsDown(87);
   if (w){
-    vely -=player.speed;
+    vely -= 1;
   }
-  let s = keyIsDown(83);
   if (s){
-    vely +=player.speed;
+    vely += 1;
   }
 
   let spd = 1;
@@ -51,6 +63,58 @@ function moveP(){
 }
 
 
+let maxSpeed = 5;
+
+function moveB(){
+
+  let velx = 0;
+  let vely = 0;
+  let a =keyIsDown(aK);
+  let d = keyIsDown(dK);
+  let w =keyIsDown(wK);
+  let s =keyIsDown(sK);
+  if (a){
+    velx -= 1;
+  }
+  if (d){
+    velx += 1;
+  }
+  if (w){
+    vely -= 1;
+  }
+  if (s){
+    vely += 1;
+  }
+
+  let spd = 0.01;
+  if (velx+vely>0){
+    spd = 0.01/(velx+vely);
+  }
+
+
+  // Apply a force to box1 when a key is pressed
+  if (a) {
+    Matter.Body.applyForce(box1, { x: box1.position.x, y: box1.position.y }, { x: -1*spd, y: 0 });
+  }
+  if (d) {
+    Matter.Body.applyForce(box1, { x: box1.position.x, y: box1.position.y }, { x: spd, y: 0 });
+  }
+  if (w) {
+    Matter.Body.applyForce(box1, { x: box1.position.x, y: box1.position.y }, { x: 0, y: -1*spd });
+  }
+  if (s) {
+    Matter.Body.applyForce(box1, { x: box1.position.x, y: box1.position.y }, { x: 0, y: spd });
+  }
+
+  // Check and limit the speed of box1
+  const velocity1 = box1.velocity;
+  const speed1 = Matter.Vector.magnitude(velocity1);
+  if (speed1 > maxSpeed) {
+    Matter.Body.setVelocity(box1, Matter.Vector.mult(Matter.Vector.normalise(velocity1), maxSpeed));
+  }
+
+}
+
 
 let border = {
   gl: true,
@@ -71,7 +135,11 @@ function barrier(unit, object){
   let a;
   let b;
   for (let pl=0; pl<=object.points.length; pl++){
-    set = subset(object.points,pl,2);
+    if (pl===object.points.length){
+      set = [object.points[pl],object.points[0]];
+    }else{
+      set = subset(object.points[pl],2);
+    }
     lna = dist(unit.x,unit.y,set[0].x,set[0].y);
     lnb = dist(unit.x,unit.y,set[1].x,set[1].y);
     ab = dist(set[0].x,set[0].y,set[1].x,set[1].y);
@@ -79,7 +147,7 @@ function barrier(unit, object){
     a = Math.sqrt(lna**2-unit.size**2);
     b = Math.sqrt(lnb**2-unit.size**2);
 
-    if(a+b >= ab){
+    if(a+b <= ab){
       fill("blue");
     }
 
@@ -119,8 +187,27 @@ function display(){
 }
 
 
+let engine;
+let box1;
+let box2;
+
 function setup() {
   display();
+
+  engine = Matter.Engine.create();
+
+  // Disable gravity by setting the gravity vector to {x: 0, y: 0}
+  engine.world.gravity.x = 0;
+  engine.world.gravity.y = 0;
+
+
+  Matter.Runner.run(engine);
+  box1 = Matter.Bodies.circle(100, 100, 25);
+  box2 = Matter.Bodies.rectangle(150, 150, 50, 50, {isStatic: true});
+  // player = Matter.Bodies.circle()
+  Matter.World.add(engine.world, [box1, box2]);
+
+
   player.x = wind.size/2;
   player.y = wind.size/2;
   player.size = player.size*wind.size;
@@ -130,9 +217,35 @@ function setup() {
 function draw() {
   // display();
   background(30);
+
+
+  moveB();
+  // Update p5.js sketch based on Matter.js physics
+  let pos1 = box1.position;
+  let pos2 = box2.position;
   fill("red");
-  moveP();
+  ellipseMode(CORNER);
+  ellipse(pos1.x, pos1.y, 50);
+  fill("blue");
+  rect(pos2.x, pos2.y, 50, 50);
+  
+  // Check for collisions (you may need more advanced collision handling)
+  if (Matter.Collision.collides(box1, box2)) {
+    // Handle collision here
+    console.log("Collision detected!");
+  }
+
+  // // Check for collision using Matter.Bounds.overlaps
+  // if (Matter.Bounds.overlaps(box1.bounds, box2.bounds)) {
+  //   // Handle collision here
+  //   console.log("Collision detected!");
+  // }
+
+
+
+  // fill("red");
+  // moveP();
   // barrier(player,border);
-  circle(player.x,player.y,player.size);
+  // circle(player.x,player.y,player.size);
   
 }
