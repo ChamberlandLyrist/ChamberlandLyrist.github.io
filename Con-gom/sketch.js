049fde786fe9963dbd95cb93b649b0e97d510e91
ChamberlandLyrist <146767709+ChamberlandLyrist@users.noreply.github.com>
2023-10-30 12:45:58 -0600

    made floodfill, fixed start problems

--- Con-gom/sketch.js
+++ Con-gom/sketch.js
@@ -13,8 +13,10 @@ function setup() {
     cellSize = height/GRID_SIZE;
   }
 }
+
 let start = false;
 let lost = false;
+
 function draw() {
   background(220);
   displayGrid();
@@ -33,11 +35,8 @@ function mousePressed() {
   // if not started, start
   if (!start){
     start = true;
-    for(let y in grid){
-      for (let x in grid){
-        grid[y][x].
-      }
-    }
+    grid = generateRandomGrid(GRID_SIZE,GRID_SIZE);
+    floodFill(x,y);
   }
   // toggleCell(x, y);   //current cell
 }
@@ -54,17 +53,43 @@ function toggleCell(x, y) {
   }
 }
 
+let flood;
+
 function activateCell(x, y){
   if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
-    if (grid[y][x].open === false) {
-      grid[y][x].open = true;
-      if(grid[y][x].bomb === 1){
+    let box = grid[y][x];
+    if (box.open === false) {
+      box.open = true;
+      if(box.bomb === 1){
         lost = true;
       }
-      else{
-// floodfill
+    }
+  }
+}
+
+function floodFill(x,y){
+  flood = [];
+  console.log("begin flood", x, y);
+  for (let shifty of shift){
+    for (let shiftx of shift){
+      let row = y+shifty;
+      let col = x+shiftx;                                                 
+      if (col >= 0 && col < GRID_SIZE && row >= 0 && row < GRID_SIZE && Math.abs(shiftx) !== Math.abs(shifty)){
+        let box = grid[row][col];  
+                          //if neither a flag or a bomb is present
+        if (!box.open && box.flag+box.bomb === 0){
+          console.log("floodfilled:",x,y);
+          flood.push({x: col, y: row});
+          activateCell(col,row);
+        }
       }
     }
+  }
+  let thisflood = flood;
+  console.log("thisflood",thisflood);
+  for (let pos of thisflood){
+    // console.log(pos);
+    floodFill(pos.x,pos.y);
   }
 }
 
@@ -72,43 +97,56 @@ function displayGrid() {
   for (let y = 0; y < GRID_SIZE; y++) {
     for (let x = 0; x < GRID_SIZE; x++) {
       let box = grid[y][x];
-      if (box.open === 1) {
+      // console.log(x,y);
+      let row = y*cellSize;
+      let col = x*cellSize;
+      // box.adj = allNeighbours(x,y);
+      if (box.open === true) {
         fill("white");
+        rect(col, row, cellSize);
+        fill("red");
+        textAlign(LEFT,TOP);
+        textSize(cellSize);
+        text(box.adj, col, row);
       }
-      else if (box.open === 0) {
+      else if (box.open === false) {
         fill("grey");
+        rect(col, row, cellSize);
       }
-      else if (box.open === "bomb"){
-        fill("grey");
-        if (lost){
-          fill("black");
-          ellipseMode(CORNER);
-          circle(x*cellSize, y*cellSize, cellSize*0.8);
-        }
-        
+      if (box.bomb === 1){
+        // if (lost){
+        fill("black");
+        ellipseMode(CORNER);
+        circle(col, row, cellSize*0.8);
+        // }
       }
-      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+      
     }
   }
 }
 
 function generateRandomGrid(cols, rows) {
   let newGrid = [];
-  let bombs = 10;
+  let bombs = 30;
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
       // check to see if it was the origin box
       if (grid[y][x].open === false){
         // make bomb if bombs left
-        if (random(100) > 50 && bombs > 0) {
+        if (random(100) > 80 && bombs >0) {
           bombs --;
-          newGrid[y].push({open: 0, adj: 0, bomb: 1, flag: 0});
+          // console.log("bomb",x,y);
+          newGrid[y].push({open: false, adj: 0, bomb: 1, flag: 0});
         }
         // don't make bomb
         else{
-          newGrid[y].push({open: 0, adj: 0, bomb: 0, flag: 0});
+          // console.log("no bomb",x,y);
+          newGrid[y].push({open: false, adj: 0, bomb: 0, flag: 0});
         }
+      }
+      else{
+        newGrid[y].push(grid[y][x]);
       }
     }
   }
@@ -146,8 +184,8 @@ function nextTurn(){
   for (let y = 0; y < GRID_SIZE; y++){
     for (let x = 0; x < GRID_SIZE; x++){
       // console.log(y,x);
-      if (grid[y][x].open === 0 && grid[y][x].flag === 0){
-        let neighbours = allNeighbours(x,y,bomb);
+      if (!grid[y][x].open && grid[y][x].flag === 0){
+        let neighbours = allNeighbours(x,y);
         let self = grid[y][x];
         neighbours -= self;
         if (self === 0){
@@ -170,25 +208,26 @@ function nextTurn(){
   return nextGrid;
 }
 
-function allNeighbours(x,y,check,precheck){
+function allNeighbours(x,y){
   // count neighbours
   let neighbours = 0;
   // look at all cells around in a 3x3 grid
   for (let shifty of shift){
     for (let shiftx of shift){
       // console.log(grid, y, x, shifty, shiftx);
-      neighbours += checkNeighbour(x+shiftx,y+shifty,check);
+      neighbours += checkNeighbour(x+shiftx,y+shifty);
     }
   }
   return neighbours;
 }
 
-function checkNeighbour(x,y,check){
+function checkNeighbour(x,y){
   // detect edge cases
   // console.log(x,y,x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE);
+  console.log(grid[y][x]);
   if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE && grid[y][x].flag === 0) {
-    let state = grid[y][x].check;
-    console.log(state);
+    let state = grid[y][x].bomb;
+    // console.log(state);
     return state;
   }
   else{
